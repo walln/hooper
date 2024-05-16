d3f9c0889943e7da85f4acf25c39bf1521904933
Nick Wall <46641379+walln@users.noreply.github.com>
2024-05-15 23:34:14 -0500

    feat(ui): added sheet separator and tooltip

--- packages/ui/src/primitives/sheet/index.tsx
+++ packages/ui/src/primitives/sheet/index.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import * as React from "react";
+
+import type { VariantProps } from "class-variance-authority";
+
 import * as SheetPrimitive from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
-import { cva, type VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn";
 
 const Sheet = SheetPrimitive.Root;
 
---
+++ packages/ui/src/primitives/tooltip/tooltip.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "../button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./";
+
+const meta: Meta<typeof TooltipContent> = {
+	component: TooltipContent,
+	args: {
+		children: "This is a tooltip.",
+	},
+	render: (args) => (
+		<TooltipProvider>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<Button variant="outline">Hover</Button>
+				</TooltipTrigger>
+				<TooltipContent {...args} />
+			</Tooltip>
+		</TooltipProvider>
+	),
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TooltipContent>;
+
+export const Default: Story = {};
