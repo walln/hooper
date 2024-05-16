646af93d83adbb66063cc53c15cdadddb50a4121
Nick Wall <46641379+walln@users.noreply.github.com>
2024-05-15 23:34:14 -0500

    feat(ui): added sheet separator and tooltip

--- packages/ui/src/primitives/separator/index.tsx
+++ packages/ui/src/primitives/separator/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import * as React from "react";
+
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn";
 
 const Separator = React.forwardRef<
 	React.ElementRef<typeof SeparatorPrimitive.Root>,
