821b542e4ff5181721dd7a7f7045b821ffd068f2
Nick Wall <46641379+walln@users.noreply.github.com>
2024-05-17 01:05:38 -0500

    style(frontend): consistent bg

--- apps/frontend/components/chat/chat-panel.tsx
+++ apps/frontend/components/chat/chat-panel.tsx
@@ -72,7 +72,7 @@ export function ChatPanel({
 						exampleMessages.map((example, index) => (
 							<div
 								key={example.heading}
-								className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 ${
+								className={`cursor-pointer rounded-lg border  p-4 hover:bg-muted ${
 									index > 1 && "hidden md:block"
 								}`}
 								onClick={async () => {
@@ -98,7 +98,7 @@ export function ChatPanel({
 								}}
 							>
 								<div className="text-sm font-semibold">{example.heading}</div>
-								<div className="text-sm text-zinc-600">
+								<div className="text-sm text-muted-foreground">
 									{example.subheading}
 								</div>
 							</div>
